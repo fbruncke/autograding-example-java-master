5d0fd7758ef034343f9158105e1df2075f19cf0f
Frank <fha@easv.dk>
2023-12-18 21:28:40 +0100

    Initial commit

---
+++ src/main/java/com/example/project/Hello.java
@@ -0,0 +1,15 @@
+package com.example.project;
+
+public class Hello {
+
+	public static void main(final String[] args) {
+
+		//System.out.println("Not the right string, tests will fail!");
+		System.out.print("Hello World!");
+
+
+
+	}
+
+}
+
